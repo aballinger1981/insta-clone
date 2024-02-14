0ff3bcf9dd38863f3c09d741bb4d35aa613a9c03
Andrew Ballinger <andrew@Andrews-MacBook-Air.local>
2024-02-14 11:50:18 -0500

    change link

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -31,7 +31,7 @@ const SuggestedUsers = () => {
       <Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
         © 2024 Built By{" "}
         <Link
-          href="https://www.instagram.com/"
+          href="https://github.com/aballinger1981"
           target="_blank"
           color={"blue.500"}
           fontSize={14}
